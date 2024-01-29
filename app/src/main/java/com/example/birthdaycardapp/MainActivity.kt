59400b3cef0c3f30abdf6357d5e52a329f0533ad
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-29 20:14:28 +0000

    Fixed issue with BirthdayCardAppTheme name

--- app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.birthdaycardapp.ui.theme.BirthdayCardAppTheme
 
@@ -20,25 +21,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BirthdayCardAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ){
-
-                }
+            BirthdayCardAppTheme  {
+                BirthdayCard(
+                        "Happy Birthday Jan",
+                        "From Fiona",
+                        painterResource(R.drawable.kitten)
+                )
             }
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun BirthdayPreview() {
-    BirthdayCardAppTheme {
-    }
-}
 
 @Composable
 fun BirthdayCard(

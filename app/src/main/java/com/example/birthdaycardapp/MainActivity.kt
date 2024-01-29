252c277145f34ee89c436f98d977b0dd30c4904b
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-29 19:27:59 +0000

    Added BirthdayCard composable with supporting imports

--- app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.birthdaycardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.birthdaycardapp.ui.theme.BirthdayCardAppTheme
 
@@ -33,6 +37,30 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthdayPreview() {
     BirthdayCardAppTheme {
-        Greeting("Android")
+    }
+}
+
+@Composable
+fun BirthdayCard(
+        message: String,
+        from: String,
+        picture: Painter,
+        modifier: Modifier = Modifier
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+                painter = picture,
+                contentDescription = null
+        )
+        Column(modifier = Modifier.fillMaxSize()){
+            Text(
+                    text = message,
+                    modifier = modifier
+            )
+            Text(
+                    text = from,
+                    modifier = modifier
+            )
+        }
     }
 }

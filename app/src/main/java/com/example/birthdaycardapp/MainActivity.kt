13921a3118af33bc969b53ad28a2dc23e34f1ab9
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-29 21:13:12 +0000

    Added transparency and size mofidier to image

--- app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,9 +45,11 @@ fun BirthdayCard(
 ) {
     Box(modifier = Modifier.fillMaxSize()) {
         Image(
-                painter = picture,
-                contentDescription = null,
-                modifier = modifier
+            painter = picture,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F,
+            modifier = Modifier.fillMaxSize()
         )
         Column(modifier = Modifier.fillMaxSize()) {
             Text(
